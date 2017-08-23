1cfc861010c4a5a78bbee1b1e707950bde104bdd
Pyrxis <oasishaon@gmail.com>
2017-08-23 15:48:13 +0300

    Create Utility.java

---
+++ main/Utility.java
@@ -0,0 +1,12 @@
+package main.util;
+
+import main.ImageLoader;
+
+public class Utility {
+	
+	public static final int SPRITE_SIZE = 64;
+	public static final int WIDTH = 1366, HEIGHT = 768 + 24;
+	public static final int UPDATE_PER_FRAME = 6;
+	public static boolean timestopped = false;
+	public static final ImageLoader im = new ImageLoader();
+}
